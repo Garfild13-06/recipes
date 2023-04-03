eb87ec46da9aba916dcc97c3d7e7f9bed982597a
Garfild13-06 <Garfild13-06@yandex.com>
2023-04-03 18:04:00 +0300

    Task 3 done

--- main.py
+++ main.py
@@ -34,3 +34,33 @@ def get_shop_list_by_dishes(dishes, person_count):
 
 
 pprint(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))
+
+
+def task_3():
+    files_len = {}
+    for i in range(1, 4):
+        path = os.path.join(os.getcwd(), 'files', f'{i}.txt')
+        with open(path, 'rt') as file:
+            content = file.readlines()
+            files_len[len(content)] = f"{i}.txt"
+
+    res_file_path = os.path.join(os.getcwd(), 'files', 'result.txt')
+    result_str = ""
+    for file in sorted(files_len):
+        path = os.path.join(os.getcwd(), 'files', files_len[file])
+        with open(path, 'rt') as f:
+            result_str += f"{files_len[file]}\n"
+            file_num = files_len[file].split('.')[0]
+            for index, line in enumerate(f):
+                result_str += f"Строка номер {index + 1} файла {file_num}\n"
+                result_str += f"{line.strip()}\n"
+    if os.path.exists(res_file_path):
+        os.remove(res_file_path)
+        with open(res_file_path, 'xt', encoding='utf-8') as f:
+            f.write(result_str)
+    else:
+        with open(res_file_path, 'xt', encoding='utf-8') as f:
+            f.write(result_str)
+
+
+task_3()
