1734c6d291b445f09b54f6cd47fa1c4c0870781a
Garfild13-06 <Garfild13-06@yandex.com>
2023-04-03 17:08:14 +0300

    Task 2 done

--- main.py
+++ main.py
@@ -19,4 +19,18 @@ with open(full_path, 'rt') as file:
         file.readline()
         cook_book[dish_name] = ingridients
 
-pprint(cook_book, sort_dicts=False)
+
+# pprint(cook_book, sort_dicts=False)
+
+def get_shop_list_by_dishes(dishes, person_count):
+    shop_list = {}
+    for dish in dishes:
+        for ingridient in cook_book[dish]:
+            shop_list[ingridient['ingredient_name']] = {
+                'measure': ingridient['measure'],
+                'quantity': (int(ingridient['quantity']) * person_count)
+            }
+    return shop_list
+
+
+pprint(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))
